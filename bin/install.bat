d0a07de7d2bb51001961ce8957369035fa00ba3f
Ranjan-Dasgupta <Ranjan.Dasgupta@us.ibm.com>
2020-02-12 09:00:52 -0600

    Updated jar file

---
+++ bin/install.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM IBM Maximo Application Suite - SCADA Bulk Data Ingest Connector
+REM *****************************************************************************
+REM Copyright (c) 2019 IBM Corporation and other Contributors.
+REM All rights reserved. This program and the accompanying materials
+REM are made available under the terms of the Eclipse Public License v1.0
+REM which accompanies this distribution, and is available at
+REM http://www.eclipse.org/legal/epl-v10.html
+REM *****************************************************************************
+REM Script to install Data Connector
+
+REM ---- TO BE ADDED -----
+
